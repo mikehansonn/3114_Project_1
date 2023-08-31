79681526973fbf74ff8a4b2e6994ea2ebc0e8342
matt02stew <matt02@vt.edu>
2023-08-30 20:50:41 -0400

    Record class

---
+++ src/Record.java
@@ -0,0 +1,36 @@
+public class Record {
+    private int length; // Length of the byte array
+    private int startPosition; // Starting position of the array in some external storage
+
+    // Constructor
+    public Record(int length, int startPosition) {
+        this.length = length;
+        this.startPosition = startPosition;
+    }
+
+    // Getters and Setters
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getStartPosition() {
+        return startPosition;
+    }
+
+    public void setStartPosition(int startPosition) {
+        this.startPosition = startPosition;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Record{" +
+                "length=" + length +
+                ", startPosition=" + startPosition +
+                '}';
+    }
+}
