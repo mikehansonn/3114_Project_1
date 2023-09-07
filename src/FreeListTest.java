08a323f836138d806fe4f0e907f4b68c87eb6d92
matt02stew <matt02@vt.edu>
2023-09-07 12:23:28 -0400

    freelist test

---
+++ src/FreeListTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FreeListTest {
+	
+	private FreeList freeList;
+
+	@Before 
+    public void setUp() throws Exception {
+        freeList = new FreeList(4); // Adjust as necessary
+    }
+	
+	@Test
+	public void testAddBlock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeallocateBlock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoubleMemory() {
+		fail("Not yet implemented");
+	}
+
+}
