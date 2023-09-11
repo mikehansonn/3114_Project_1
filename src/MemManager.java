28ef093a69b67711a423775b561e9eeb78c51f21
matt02stew <matt02@vt.edu>
2023-09-11 19:41:06 -0400

    commenting and testing

--- src/MemManager.java
+++ src/MemManager.java
@@ -1,23 +1,42 @@
-
+/**
+ * This is a Memory Manager implementation for the project
+ * 
+ * @author mikehanson, matt02
+ * @version 9/11/23
+ * 
+ */
 public class MemManager {
-	private int poolsize; 
-	private byte[] memoryPool;
-	private FreeList freeList; 
+	private int poolsize; // the size of the memory pool
+	private byte[] memoryPool; // byte array the holds seminar information in byte form
+	private FreeList freeList; // freelist that determines free space in the byte array 
 	
-	// Constructor. poolsize defines the size of the memory pool in bytes
+	/**
+     * Constructs a new MemManager with a specified memory pool size.
+     * It also initializes the free list with the highest power of 2 equal to poolsize.
+     *
+     * @param poolsize the initial size of the memory pool in bytes
+     */
 	public MemManager(int poolsize) { 
 		this.poolsize = poolsize;    
 		this.memoryPool = new byte[poolsize]; 
 		
-		int sizePower = (int) (Math.log(poolsize) / Math.log(2));
+		int sizePower = (int) (Math.log(poolsize) / Math.log(2)); // finds highest power of 2 equal to poolsize 
 		this.freeList = new FreeList(sizePower); 
 		
 	}
 	
+	/**
+     * Gets the current size of the memory pool.
+     *
+     * @return the size of the memory pool in bytes
+     */
 	public int getPoolsize() {
 		return poolsize;
 	}
 	
+	/**
+     * Doubles the size of the memory pool, copying existing data to the new, larger pool.
+     */
 	private void doubleSize() { 
         byte[] newMemoryPool = new byte[poolsize * 2];
         poolsize *= 2;
@@ -27,45 +46,58 @@ public class MemManager {
         freeList.doubleMemory();   
     }
 	
-	// Insert a record and return its position handle.
-	// space contains the record to be inserted, of length size.
+	/**
+     * Inserts a new record into the memory pool, returning a handle to its location.
+     *
+     * @param space the data to be inserted
+     * @param size the number of bytes to insert
+     * @return a handle pointing to the record's location
+     */
 	public Handle insert(byte[] space, int size) {
 		int sizePower = (int) Math.ceil(Math.log(size) / Math.log(2));
 		
 		int startPosition; 
 		
+		// doubles pool size until the data to be added can fit
+		// applies when the first element added is to large
 		while((1 << sizePower > poolsize)) {
 			doubleSize(); 
 		}
-			
+		
 		startPosition = freeList.addBlock(sizePower);
 			 
-		
+		// if there is no block to fit the inserted data it will keep doubling
+		// until there is space
 		while (startPosition == -1) { 
 	        doubleSize();
 	        startPosition = freeList.addBlock(sizePower);
 	    }
 		
+		// copies data into byte array
 		System.arraycopy(space, 0, memoryPool, startPosition, size);
-		
+
 		Handle handle = new Handle(size, startPosition);
 		
 		return handle;
 	}
 	
 	
-	// Return the length of the record associated with theHandle
+	/**
+     * Gets the length of the record associated with the given handle.
+     *
+     * @param theHandle the handle pointing to the record
+     * @return the length of the record in bytes
+     */
 	public int length(Handle theHandle) { 
-		if (theHandle == null) {
-	        return -1;  // Or any other value/error message to indicate that the handle is null
-	    }
-		
 		return theHandle.getLength();
 	}
 	 
 	
-	// Free a block at the position specified by theHandle.
-	// Merge adjacent free blocks.
+	/**
+     * Removes the record associated with the given handle, resetting its bytes to zero 
+     *
+     * @param theHandle the handle pointing to the record to be removed
+     */
 	public void remove(Handle theHandle) { 
 		
 		int startPosition = theHandle.getStartPosition();
@@ -85,11 +117,18 @@ public class MemManager {
 	}
 	
 	
-	// Return the record with handle posHandle, up to size bytes, by
-	// copying it into space.
-	// Return the number of bytes actually copied into space.
+	 /**
+     * Retrieves a record associated with the given handle, copying up to a specified 
+     * number of bytes into a provided array.
+     *
+     * @param space the array to copy the data into
+     * @param theHandle the handle pointing to the record to retrieve
+     * @param size the maximum number of bytes to retrieve
+     * @return the actual number of bytes copied into the provided array
+     */
 	public int get(byte[] space, Handle theHandle, int size) { 
 		
+		// copies bytes based off the handle into the space array
 		System.arraycopy(memoryPool, theHandle.getStartPosition(), space, 0, size);
 		
 		int totalBytes = space.length; 
@@ -98,7 +137,9 @@ public class MemManager {
 		
 	}
 	
-	// Dump a printout of the freeblock list
+	 /**
+     * Dumps a printout of the current state of the free list to the console.
+     */
 	public void dump() { 
 		System.out.print(freeList.toString());
 	}
--- src/MemManagerTest.java
+++ src/MemManagerTest.java
@@ -1,8 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+
+/**
+* JUnit tests for the FreeList
+* 
+* @author mikehanson and matt02
+* @version 9/11/23
+*/
 public class MemManagerTest {
     
+	
+	/**
+	 * Tests Insert method and getLength
+	 */
 	@Test
     public void testInsertAndLength() {
         MemManager memManager = new MemManager(64); // Initialize with a 64-byte memory pool
@@ -15,7 +26,10 @@ public class MemManagerTest {
         // Check if the length of the inserted data is correct
         assertEquals(dataSize, memManager.length(handle));
     } 
-
+	
+	/**
+	 * Tests Insert method and getData
+	 */
     @Test
     public void testInsertAndGetData() {
         MemManager memManager = new MemManager(64); // Initialize with a 64-byte memory pool
@@ -33,7 +47,26 @@ public class MemManagerTest {
         assertArrayEquals(data, retrievedData);
         assertEquals(dataSize, bytesRead);
     }
+    
+    /**
+	 * Tests insert method upon doubling case
+	 */
+    @Test
+    public void testInsert_DoubleCase() {
+    	 MemManager memManager = new MemManager(4);
+    	 byte[] data = {1, 2, 3, 4};
+         byte[] data1 = {1, 2, 3};
+         
+         memManager.insert(data, data.length);
+         memManager.insert(data1, data1.length);
+         
+         // Check if the memory pool size has doubled
+         assertEquals(8, memManager.getPoolsize());
+    }
 
+    /**
+	 * Tests the remove() method
+	 */
     @Test
     public void testRemove() {
         MemManager memManager = new MemManager(64); // Initialize with a 64-byte memory pool
@@ -52,32 +85,36 @@ public class MemManagerTest {
 
         // Check if the data was successfully removed
         assertEquals(dataSize, bytesRead);
-    
     }
 
+    /**
+	 * Tests DoubleSize method 
+	 */
     @Test
     public void testDoubleSize() {
-		System.out.println("-------------------");
         MemManager memManager = new MemManager(4);
-        //byte[] data = {1, 2, 3, 4};
         byte[] data1 = {1, 2, 3, 4, 5};
-        //int dataSize = data.length;
-        
-        // Insert data into the memory pool that exceeds its initial size
-        //memManager.insert(data, dataSize);
         
         memManager.insert(data1, data1.length);
+        
         // Check if the memory pool size has doubled
         assertEquals(8, memManager.getPoolsize());
-        
-        
     }
     
+    /**
+	 * Tests dump() method
+	 * Most cases tested in Freelist toString() test
+	 */
     @Test
     public void testDump() {
+    	// Test with no elements
+    	MemManager memManager = new MemManager(8);
+    	memManager.dump(); 
     	
-    	
-    	
-        
+    	// test with with empty freelist
+    	byte[] data1 = {1, 2, 3, 4, 5};
+    	memManager.insert(data1, data1.length);
+    	memManager.dump();
     }
+    
 }
